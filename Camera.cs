8397fd8984472d47f522e4cb2823fbd7d8274e85
Felipe Yépez <58478476+FelipeYepez@users.noreply.github.com>
2020-12-16 13:02:32 -0500

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform ball;
+    public float smoothSpeed = 0.125f;
+    float ballX, ballY;
+
+    void Start()
+    {
+        ballX = ball.position.x;
+        ballY = ball.position.y;
+    }
+    
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(ballX, ballY, ball.position.z - 10f);
+        transform.position = desiredPosition;
+    }
+}
